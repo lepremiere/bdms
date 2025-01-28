38fedd27860d0207cc587acd69d875c8a66da9e0
Henry <huick.henry@googlemail.com>
2025-01-28 16:59:14 +0100

    Sets up clean module

---
+++ bdms/clean.py
@@ -0,0 +1,15 @@
+
+
+def clean_database(
+        root_dir: str,
+    ) -> None:
+    """
+    Clean the database by removing daily elements if monthly elements exist,
+    that contain the same data.
+    
+    Parameters
+    ----------
+    root_dir : str
+        The root directory of the database.
+    """
+    raise NotImplementedError
