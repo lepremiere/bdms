b22c2e9e9670c9db6455940ad3e76baf74a02637
Henry <huick.henry@googlemail.com>
2025-01-28 17:02:04 +0100

    Updates main.py

--- main.py
+++ main.py
@@ -5,34 +5,31 @@ if __name__ == "__main__":
         "BTCUSDT", "BNBUSDT", "ADAUSDT", 
         "ETHUSDT", "XRPUSDT", "SOLUSDT"
     ]
-    market_data_types = [
-        "klines", "fundingRate", 
+    download_market_data_types = [
+        "aggTrades", "klines", "fundingRate", 
         "indexPriceKlines", "markPriceKlines", 
-        "premiumIndexKlines", "aggTrades"
+        "premiumIndexKlines", "bookDepth", "metrics",
     ]
     bdms.populate_database(
-        root_dir="D:/Binance",
+        root_dir="D:/Binance1",
         symbols=symbols, 
-        trading_types=["spot", "um"], 
-        market_data_types=market_data_types,
+        trading_types=["spot"], 
+        market_data_types=download_market_data_types,
         intervals=["1m"],
         storage_format="parquet",
     )
-    # bdms.merge_trading_data(
-    #     symbols=["ADAUSDT"], 
-    #     trading_type="spot", 
-    #     market_data_type="aggTrades",
-    #     time_period="monthly",
-    #     root_dir="D:/Binance",
-    #     output_dir="D:/Binance",
-    #     start_date="2024-12-01",
-    #     end_date="2024-12-01",
-    #     data_base_format="parquet",
-    #     output_format="parquet",
-    # )
-    # bdms.update_aggTrades(
-    #     api_key=api_key,
-    #     api_secret=api_secret,
-    #     path="D:/Binance/ADAUSDT.parquet",
-    # )
-
+    
+    agg_market_data_types = [
+        "klines", "fundingRate", 
+        "indexPriceKlines", "markPriceKlines", 
+        "premiumIndexKlines", "bookDepth", "metrics",
+    ]
+    bdms.merge_database(
+        root_dir="D:/Binance",
+        symbols=symbols, 
+        trading_types=["spot"], 
+        market_data_types=agg_market_data_types,
+        intervals=["1m"],
+        data_base_format="parquet",
+        output_format="parquet",
+    )
