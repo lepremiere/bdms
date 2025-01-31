4ddb057042ea5198842269000f711bb54bd6ccb6
Henry <huick.henry@googlemail.com>
2025-01-31 15:00:04 +0100

    Updates main.py

--- main.py
+++ main.py
@@ -8,33 +8,29 @@ if __name__ == "__main__":
         "BTCUSDT", "BNBUSDT", "ADAUSDT", 
         "ETHUSDT", "XRPUSDT", 
     ]
-    download_market_data_types = [
+    market_data_types = [
         "aggTrades", "klines", "fundingRate", 
         "indexPriceKlines", "markPriceKlines", 
         "premiumIndexKlines", "bookDepth", "metrics",
     ]
-    intevals = ["1m", "5m", "1h", "1d"]
+    intervals = ["1m", "5m", "1h", "1d"]
     
     bdms.populate_database(
         root_dir="D:/Binance",
         symbols=symbols, 
-        trading_types=["um"], 
-        market_data_types=download_market_data_types,
-        intervals=intevals,
+        trading_types=["spot", "um"], 
+        market_data_types=market_data_types,
+        intervals=intervals,
         storage_format="parquet",
     )
     
-    agg_market_data_types = [
-        "klines", "fundingRate", 
-        "indexPriceKlines", "markPriceKlines", 
-        "premiumIndexKlines", "bookDepth", "metrics",
-    ]
     bdms.merge_database(
         root_dir="D:/Binance",
         symbols=symbols, 
         trading_types=["spot", "um"], 
-        market_data_types=agg_market_data_types,
-        intervals=intevals,
+        market_data_types=market_data_types,
+        intervals=intervals,
         data_base_format="parquet",
         output_format="parquet",
+        start_date="2020-01-01",
     )
